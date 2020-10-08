81b92ccc93ce8608d2211bb87bb17714aa3ae304
Ines Montani <ines@ines.io>
2020-10-08 14:49:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ spacy_streamlit.visualize(
     DEFAULT_TEXT,
     visualizers=["parser", "ner", "similarity", "tokens"],
     show_visualizer_select=True,
+    # TODO: enable when morph issue is fixed in next nightly
+    show_json_doc=False,
     sidebar_description=DESCRIPTION
 )
