b84748864088b01ff213d8dc2e4e3b62b170dec4
Ines Montani <ines@ines.io>
2020-10-08 23:32:07 +0200

    Update app

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from pathlib import Path
 import srsly
 
 MODELS = srsly.read_json(Path(__file__).parent / "models.json")
+DEFAULT_MODEL = "en_core_web_sm"
 DEFAULT_TEXT = "Sundar Pichai is the CEO of Google."
 DESCRIPTION = """**Explore trained [spaCy v3.0](https://nightly.spacy.io) pipelines**"""
 
 spacy_streamlit.visualize(
     MODELS,
     DEFAULT_TEXT,
+    default_model=DEFAULT_MODEL,
     visualizers=["parser", "ner", "similarity", "tokens"],
     show_visualizer_select=True,
-    # TODO: enable when morph issue is fixed in next nightly
-    show_json_doc=False,
     sidebar_description=DESCRIPTION
 )
