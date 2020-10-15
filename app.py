223fa88723081fa032a009245776e47c5acc1f89
Ines Montani <ines@ines.io>
2020-10-15 19:07:07 +0200

    Update app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import srsly
 
 MODELS = srsly.read_json(Path(__file__).parent / "models.json")
 DEFAULT_MODEL = "en_core_web_sm"
-DEFAULT_TEXT = "Sundar Pichai is the CEO of Google."
+DEFAULT_TEXT = "David Bowie moved to the US in 1974, initially staying in New York City before settling in Los Angeles."
 DESCRIPTION = """**Explore trained [spaCy v3.0](https://nightly.spacy.io) pipelines**"""
 
 spacy_streamlit.visualize(
@@ -13,7 +13,5 @@ spacy_streamlit.visualize(
     default_model=DEFAULT_MODEL,
     visualizers=["parser", "ner", "similarity", "tokens"],
     show_visualizer_select=True,
-    # TODO: enable when morph issue is fixed in next nightly
-    # show_json_doc=False,
-    sidebar_description=DESCRIPTION
+    sidebar_description=DESCRIPTION,
 )
