9cd80f172480a53fc3bceaa44fcb4da43debab29
Ines Montani <ines@ines.io>
2021-01-27 19:01:33 +1100

    Update versions

--- update_data.py
+++ update_data.py
@@ -13,8 +13,8 @@ URL_TEMPLATE = "https://github.com/explosion/spacy-models/releases/download/{nam
 
 def main(
     # fmt: off
-    spacy_version: str = typer.Argument(">=3.0.0rc3,<3.1.0", help="The spaCy version range"),
-    spacy_streamlit_version: str = typer.Argument(">=1.0.0rc0,<1.1.0", help="The version range of spacy-streamlit"),
+    spacy_version: str = typer.Argument(">=3.0.0rc5,<3.1.0", help="The spaCy version range"),
+    spacy_streamlit_version: str = typer.Argument(">=1.0.0rc1,<1.1.0", help="The version range of spacy-streamlit"),
     req_path: Path = typer.Option(Path(__file__).parent / "requirements.txt", "--requirements-path", "-rp", help="Path to requirements.txt"),
     desc_path: Path = typer.Option(Path(__file__).parent / "models.json", "--models-json-path", "-mp", help="Path to models.json with model details for dropdown"),
     package: str = typer.Option("spacy-nightly", "--package", "-p", help="The parent package (spacy, spacy-nightly, etc.)"),
