f91b4ff338584a9af2b10b7cfa05a7c0d7986cc6
Haowen Jiang <howard.haowen@gmail.com>
2022-01-10 00:12:34 +0800

    Change Japanese default regex

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ ZH_REGEX = "\d{2,4}"
 EN_TEXT = "(CNN) Covid-19 hospitalization rates among children are soaring in the United States, with an average of 4.3 children under 5 per 100,000 hospitalized with an infection as of the week ending January 1, up from 2.6 children the previous week, according to data from the US Centers for Disease Control and Prevention. This represents a 48% increase from the week ending December 4, and the largest increase in hospitalization rate this age group has seen over the course of the pandemic."
 EN_REGEX = "(ed|ing)$"
 JA_TEXT = "（朝日新聞）新型コロナウイルスの国内感染者は9日、新たに8249人が確認された。2日連続で8千人を超えたのは昨年9月11日以来、約4カ月ぶり。全国的に感染拡大が進む中、年をまたいだ1週間の感染者の過半数が30代以下だった。コロナ特措法に基づく「まん延防止等重点措置」が9日から適用された3県では、広島で過去最多の619人が確認された。"
-JA_REGEX = "[がでに]"
+JA_REGEX = "[たい]$"
 DESCRIPTION = "spaCy自然語言處理模型展示"
 
 st.set_page_config(
